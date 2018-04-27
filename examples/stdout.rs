71fbaa75f8c160a7f147dc84899677e123542807
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-04-27 21:09:01 +0100

    Fixup example to emit colour.

--- examples/stdout.rs
+++ examples/stdout.rs
@@ -1,3 +1,4 @@
+extern crate term;
 extern crate vga_framebuffer;
 
 struct Dummy {
@@ -23,13 +24,40 @@ impl<'a> vga_framebuffer::Hardware for &'a mut Dummy {
     }
 
     /// Called when pixels need to be written to the output pin.
-    fn write_pixels(&mut self, pixels: u16) {
-        for bit in (0..16).rev() {
-            if pixels & (1 << bit) != 0 {
-                print!("@@");
-            } else {
-                print!("..");
+    fn write_pixels(&mut self, red: u8, green: u8, blue: u8) {
+        let mut t = term::stdout().unwrap();
+        for bit in (0..8).rev() {
+            let red_bit = red & (1 << bit) != 0;
+            let blue_bit = blue & (1 << bit) != 0;
+            let green_bit = green & (1 << bit) != 0;
+            let colour: u8 = ((red_bit as u8) << 2) + ((green_bit as u8) << 1) + (blue_bit as u8);
+            match colour {
+                6 => {
+                    t.fg(term::color::YELLOW).unwrap();
+                }
+                5 => {
+                    t.fg(term::color::MAGENTA).unwrap();
+                }
+                4 => {
+                    t.fg(term::color::GREEN).unwrap();
+                }
+                3 => {
+                    t.fg(term::color::CYAN).unwrap();
+                }
+                2 => {
+                    t.fg(term::color::GREEN).unwrap();
+                }
+                1 => {
+                    t.fg(term::color::BLUE).unwrap();
+                }
+                0 => {
+                    t.fg(term::color::BLACK).unwrap();
+                }
+                _ => {
+                    t.fg(term::color::WHITE).unwrap();
+                }
             }
+            write!(t, "@@").unwrap();
         }
         self.col += 1;
         if self.col == vga_framebuffer::HORIZONTAL_WORDS {
