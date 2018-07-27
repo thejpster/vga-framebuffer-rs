940dca875a1d58e7eb8938379fb3c3fe08cb0cfc
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2018-07-27 21:27:31 +0100

    Handle 8x8 fonts.

--- font_testing/font_display.py
+++ font_testing/font_display.py
@@ -10,7 +10,13 @@ file = sys.argv[1]
 with open(file, "rb") as f:
 	data = f.read()
 
-chunk_size = int((8 * 16) / 8)
+if "8x8" in file:
+	chunk_size = int((8 * 8) / 8)
+elif "8x16" in file:
+	chunk_size = int((8 * 16) / 8)
+else:
+	raise Exception("Can't understand filename")
+
 glyphs = [data[i:i + chunk_size] for i in range(0, len(data), chunk_size)]
 
 for no, glyph in enumerate(glyphs):
@@ -18,7 +24,7 @@ for no, glyph in enumerate(glyphs):
 	name = unicodedata.name(uni[0], "UNKNOWN")
 	camel_name = "".join(map(str.title, name.replace("-", " ").split()))
 	print("*" * 10)
-	print("Glyph {} ({}) = {} (Glyph::{})\n".format(no, uni, name, camel_name))
+	print("Glyph {} ({}) = {} (Glyph::{})".format(no, uni, name, camel_name))
 	print("    {}, // Glyph::{}".format(",".join(map(lambda x: "0x%02x" % x, glyph)), camel_name))
 	for row in glyph:
 		for bit in range(0, 8):
