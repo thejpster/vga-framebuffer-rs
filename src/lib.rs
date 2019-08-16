9063e915606fe9878b5c8c685acf0584a1346525
Jonathan 'theJPster' Pallant <github@thejpster.org.uk>
2019-08-16 20:38:38 +0100

    Remove the roller buffer, but leave a dummy API.

--- src/lib.rs
+++ src/lib.rs
@@ -396,8 +396,6 @@ where
     frame: usize,
     // Add one extra row because 600 doesn't divide by 16
     text_buffer: [TextRow; TEXT_NUM_ROWS + 1],
-    // Allows us to map any visible line to any other visible line.
-    roller_buffer: [u16; USABLE_LINES],
     hw: Option<T>,
     attr: Attr,
     pos: Position,
@@ -423,7 +421,6 @@ where
                     double_height: DoubleHeightMode::Normal,
                     glyphs: [(Char::Null, DEFAULT_ATTR); TEXT_NUM_COLS],
                 }; TEXT_NUM_ROWS + 1],
-                roller_buffer: [0; USABLE_LINES],
                 hw: None,
                 pos: Position {
                     row: Row(0),
@@ -451,9 +448,6 @@ where
             PIXEL_CLOCK,
         );
         self.hw = Some(hw);
-        for (idx, line) in self.roller_buffer.iter_mut().enumerate() {
-            *line = idx as u16;
-        }
         self.clear();
     }
 
@@ -520,12 +514,8 @@ where
         }
     }
 
-    pub fn map_line(&mut self, visible_line: u16, rendered_line: u16) {
-        if (rendered_line as usize) < USABLE_LINES {
-            if let Some(n) = self.roller_buffer.get_mut(visible_line as usize) {
-                *n = rendered_line;
-            }
-        }
+    pub fn map_line(&mut self, _visible_line: u16, _rendered_line: u16) {
+        // Do nothing
     }
 
     /// Returns the current frame number.
@@ -607,7 +597,6 @@ where
     /// triplets to the callback function (to be buffered).
     fn calculate_pixels(&mut self) {
         let real_line = self.line_no.load(Ordering::Relaxed) - V_DATA_FIRST;
-        let line = self.roller_buffer[real_line as usize] as usize;
         let text_row = line / MAX_FONT_HEIGHT;
         let row = &self.text_buffer[text_row];
         let font_row = match row.double_height {
